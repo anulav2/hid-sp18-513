5fc388ba7e66305ed2be133ec7784d341e2ee913
UMAKUGAN <31508874+UMAKUGAN@users.noreply.github.com>
2018-02-05 07:37:53 -0700

    Update run.py

--- REST/run.py
+++ REST/run.py
@@ -1,1 +1,15 @@
+from eve import Eve
+import platform
 
+app = Eve()
+
+
+@app.route('/ubuntu/processor',methods=['GET'])
+
+def processor():
+    ProcessorName = platform.processor()
+    return "Processor name: " + ProcessorName + "\n"
+
+
+if __name__ == '__main__':
+    app.run()
