46af1b87414c13f8558942f9dec41cbbdade9e57
UMAKUGAN <31508874+UMAKUGAN@users.noreply.github.com>
2018-04-27 04:07:15 -0700

    Create req_validation.py

---
+++ project-code/req_validation.py
@@ -0,0 +1,47 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+import sqlite3
+from sqlite3 import Error
+from datetime import datetime
+import json
+import logging
+
+app = Flask(__name__)
+api = Api(app)
+
+def validate_action():
+    """ Update Status """
+    APPROVED_QUERY = '''UPDATE user_req_access set req_status = 'APPROVED'
+                        WHERE EXISTS (SELECT 1 FROM Usergrp WHERE user_req_access.tgt_user_id = usergrp.user_id
+                        AND user_req_access.tgt_host_name = usergrp.server)
+                        AND req_status = 'INITIAL_REQUEST';'''
+    DECLINED_QUERY = '''UPDATE user_req_access set req_status = 'DECLINED',
+                        req_reject_reason = 'User doesnot have access to Targer Server'
+                        WHERE NOT EXISTS (SELECT 1 FROM Usergrp WHERE user_req_access.tgt_user_id = usergrp.user_id
+                        AND user_req_access.tgt_host_name = usergrp.server)
+                        AND req_status = 'INITIAL_REQUEST';'''
+    REQUEST_COUNT_QUERY = '''SELECT COUNT(1) FROM user_req_access WHERE req_status = 'INITIAL_REQUEST';'''
+    with sqlite3.connect('sshkeymgmt.db') as conn:
+        c = conn.cursor()
+        c.execute(REQUEST_COUNT_QUERY)
+        results = c.fetchall()
+        print('Number of requests to be processed: {0}', results)
+        c.execute(APPROVED_QUERY)
+        c.execute(DECLINED_QUERY)
+        c.execute('''SELECT * FROM user_req_access;''')
+        results = c.fetchall()
+        print(results)
+        
+class validate_and_action(Resource):
+    """
+    """
+    def post(self):
+       logging.info('Validating Request')
+       validate_action()
+
+api.add_resource(validate_and_action,'/validate_and_action',methods=['POST'])
+
+if __name__ == '__main__':
+   logging.getLogger().setLevel(logging.DEBUG)
+   logging.info('Validating Access Request')
+   app.run(host='0.0.0.0', debug=True)
