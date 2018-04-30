def6d28ae6152e94f4c37e54fb1835deb3d8761f
UMAKUGAN <31508874+UMAKUGAN@users.noreply.github.com>
2018-04-30 15:58:22 -0700

    Update create_ssh_key.py

--- project-code/create_ssh_key.py
+++ project-code/create_ssh_key.py
@@ -56,5 +56,5 @@ api.add_resource(create_ssh_key,'/create_ssh_key',methods=['POST'])
 if __name__ == "__main__":
    logging.getLogger().setLevel(logging.DEBUG)
    logging.info('Create SSH Key')
-   app.run(host='0.0.0.0', debug=True)
+   app.run(host='0.0.0.0',port=5001,debug=True)
 
